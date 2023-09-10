103b80fe41ef09de12d554ccb6a316b900a30db5
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-10 20:50:36 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,3 +1,5 @@
-class fifo_driver extends uvm_driver #(fifo_sequence_item);
-
+class fifo_driver extends uvm_driver #(fifo_transaction);
+ virtual fifo_interface vif;
+  fifo_transaction req;
+  
 endclass : fifo_driver
