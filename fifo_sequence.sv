b3b927c85837354fba17509dc2fe343cad0490bf
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-10 20:47:52 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,6 +1,6 @@
 class fifo_sequence extends uvm_sequence #(fifo_transaction);
   `uvm_object_utils(fifo_sequence) //factory registration
-   fifo_sequence_item req;
+   fifo_transaction req;
   function new(string name = "fifo_sequence");
     super.new(name);
   endfunction
@@ -8,7 +8,7 @@ class fifo_sequence extends uvm_sequence #(fifo_transaction);
 virtual task body();
   `uvm_info(get_type_name(), $sformatf("******** Generate 100 Write REQs ********"), UVM_LOW)
   repeat(100) begin
-     req = fifo_sequence_item::type_id::create("req");  //creating sequence_item
+     req = fifo_transaction::type_id::create("req");  //creating sequence_item
     wait_for_grant(); // these steps can be repalced by start_item and finifh_item
      assert(req.randomize()with {i_wren==1;});
      send_request(req);
@@ -18,7 +18,7 @@ virtual task body();
   
   `uvm_info(get_type_name(), $sformatf("******** Generate 100 Read REQs ********"), UVM_LOW)
   repeat(100) begin
-     req = fifo_sequence_item::type_id::create("req");  //creating sequence_item
+     req = fifo_transaction::type_id::create("req");  //creating sequence_item
      wait_for_grant();
      assert(req.randomize()with {i_rden==1;});
      send_request(req);
@@ -28,7 +28,7 @@ virtual task body();
   
   `uvm_info(get_type_name(), $sformatf("******** Generate Random REQs ********"), UVM_LOW)
   repeat(100) begin
-     req = fifo_sequence_item::type_id::create("req");  //creating sequence_item
+     req = fifo_transaction::type_id::create("req");  //creating sequence_item
      wait_for_grant();
      assert(req.randomize());
      send_request(req);
