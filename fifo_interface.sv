55cfa1adf4669d4843a02c40d95e5f077e599a23
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-10 12:38:28 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -1,5 +1,5 @@
-interface intf;
-  logic clk,rstn,i_wren,i_rden,o_full,o_empty,o_alm_full,o_alm_empty;
+interface intf(clk,rstn);
+  logic i_wren,i_rden,o_full,o_empty,o_alm_full,o_alm_empty;
   logic  [DATA_W - 1 : 0] i_wrdata;
   logic  [DATA_W - 1 : 0] o_rddata;
 endinterface : intf
