e093b9bde155ab5d17626743c56344b5a7ffcd15
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-10 20:54:47 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,5 +1,17 @@
 class fifo_driver extends uvm_driver #(fifo_transaction);
- virtual fifo_interface vif;
-  fifo_transaction req;
+ virtual fifo_intf vif; //interface handle
+  fifo_transaction req; //sequence_item handle
+ 
+ `uvm_utils_component(fifo_driver)
+ 
+ function new(string name = "fifo_driver", uvm_component parent);
+    super.new(name, parent);
+  endfunction
   
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+   if(!uvm_config_db#(virtual fifo_intf)::get(this, "", "vif", vif))
+      `uvm_fatal("Driver: ", "No vif is found!")
+  endfunction
+
 endclass : fifo_driver
