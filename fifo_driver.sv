83c0ec42ab6e26f82e56be6151d23c9ffb67b9c3
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-10 21:01:56 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -7,11 +7,16 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
  function new(string name = "fifo_driver", uvm_component parent);
     super.new(name, parent);
   endfunction
-  
+
+ // Build Phase
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
    if(!uvm_config_db#(virtual fifo_intf)::get(this, "", "vif", vif))
       `uvm_fatal("Driver: ", "No vif is found!")
   endfunction
 
+// Run phase  
+    virtual task run_phase(uvm_phase phase);
+
+   endtask
 endclass : fifo_driver
