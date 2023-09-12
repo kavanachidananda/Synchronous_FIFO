5eb96610186841dd4d91a86740d7e4f762826839
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 16:01:14 +0530

    Update fifo_output_monitor.sv

--- fifo_output_monitor.sv
+++ fifo_output_monitor.sv
@@ -1,10 +1,17 @@
-class fifo_output_monitor extends uvm_monitor;
+class fifo_output_monitor extends uvm_monitor; 
   virtual fifo_intf vif;
-  fifo_transaction item_got;
-  uvm_analysis_port#(f_sequence_item) item_got_port;
-  `uvm_component_utils(f_monitor)
+  fifo_transaction req;
+   uvm_analysis_port#(fifo_transaction) ap;
+  `uvm_component_utils(fifo_output_monitor)
   
-  function new(string name = "f_monitor", uvm_component parent);
+   function new(string name = "fifo_output_monitor", uvm_component parent);
     super.new(name, parent);
-    item_got_port = new("item_got_port", this);
+     ap = new("analysis_port", this);
+  endfunction
+
+   virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+     req = f_sequence_item::type_id::create("req");
+      if(!uvm_config_db#(virtual fifo_intf)::get(this, "", "vif", vif))
+      `uvm_fatal("Monitor: ", "No vif is found!")
   endfunction
