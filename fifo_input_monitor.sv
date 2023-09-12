741a97c15883545332922ca0f96a4b2805a0e2e4
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 15:46:52 +0530

    Update fifo_input_monitor.sv

--- fifo_input_monitor.sv
+++ fifo_input_monitor.sv
@@ -1,8 +1,8 @@
-class fifo_output_monitor extends uvm_monitor;
+class fifo_input_monitor extends uvm_monitor;
   virtual fifo_intf vif;
   fifo_transaction req;
    uvm_analysis_port#(fifo_transaction) ap;
-   `uvm_component_utils(fifo_output_monitor)
+  `uvm_component_utils(fifo_input_monitor)
   
    function new(string name = "fifo_output_monitor", uvm_component parent);
     super.new(name, parent);
