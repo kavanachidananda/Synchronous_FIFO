f72cd7d798f5fd04a9d92cd0f4d95d0ec680157d
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 16:39:38 +0530

    Update fifo_env.sv

--- fifo_env.sv
+++ fifo_env.sv
@@ -1,3 +1,23 @@
-class fifo_env extends uvm_env;
-
+`include "fifo_active_agent.sv"
+`include "fifo_scoreboard.sv"
+ class fifo_env extends uvm_env;
+  fifo_agent agt;
+  fifo_scoreboard scb;
+   `uvm_component_utils(fifo_env)
+  
+   function new(string name = "fifo_env", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    agt = fifo_active_agent::type_id::create("agt", this);
+    scb = fifo_scoreboard::type_id::create("scb", this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    f_agt.f_mon.item_got_port.connect(f_scb.item_got_export);
+  endfunction
+  
+endclass
 endclass : fifo_env
