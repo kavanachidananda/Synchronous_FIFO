b59ec067903a0614b5c442cbd45304e43aa42a2d
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 13:02:02 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -24,14 +24,19 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
    endtask
     forever begin
       seq_item_port.get_next_item(req);
-     if(req.i_wren == 1)
+     if(req.i_wren == 1 && req.i_rden == 0)
       write(req.i_wrdata);
-     else if(req.i_rden == 1)
+     if(req.i_wren == 0 && req.i_rden == 1)
         read();
+     if(req.i_rden == 1 && req.i_wren == 1)
+       write_and_read(req.i_wrdata);
+    else
+     idle();
       seq_item_port.item_done();
     end
   endtask : run_phase
 
+   
    virtual task write(input  [DATA_W - 1 : 0] i_wrdata);
     @(posedge vif.d_mp.clk)
     vif.d_mp.dcb.i_wren <= 1;
@@ -40,11 +45,26 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
    // vif.d_mp.dcb.i_wren <= 0;
   endtask : write
 
-      virtual task read();
+   virtual task read();
     @(posedge vif.d_mp.clk)
     vif.d_mp.dcb.i_rden <= 1;
   //  @(posedge vif.d_mp.clk)
    // vif.d_mp.dcb.i_rden <= 0;
   endtask : read
+
+   virtual task write_and_read(input  [DATA_W - 1 : 0] i_wrdata);
+     @(posedge vif.d_mp.clk)
+    vif.d_mp.dcb.i_wren <= 1;
+    vif.d_mp.dcb.i_wrdata <= i_wrdata;
+    vif.d_mp.dcb.i_rden <= 1;
+   endtask : write_and_read
+
+   virtual task idle(input  [DATA_W - 1 : 0] i_wrdata);
+     @(posedge vif.d_mp.clk)
+    vif.d_mp.dcb.i_wren <= 0;
+    vif.d_mp.dcb.i_wrdata <= i_wrdata;
+    vif.d_mp.dcb.i_rden <= 0;
+   endtask : write_and_read
+
    
 endclass : fifo_driver
