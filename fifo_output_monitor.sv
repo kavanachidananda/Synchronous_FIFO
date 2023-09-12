ea8ae4c8dc50cff083cac8e9366c558f5a62a2ba
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 16:27:15 +0530

    Update fifo_output_monitor.sv

--- fifo_output_monitor.sv
+++ fifo_output_monitor.sv
@@ -9,11 +9,21 @@ class fifo_output_monitor extends uvm_monitor;
      ap = new("analysis_port", this);
   endfunction
 
-   virtual function void build_phase(uvm_phase phase);
+  virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
      req = f_sequence_item::type_id::create("req");
       if(!uvm_config_db#(virtual fifo_intf)::get(this, "", "vif", vif))
       `uvm_fatal("Monitor: ", "No vif is found!")
   endfunction
 
+  virtual task run_phase(uvm_phase phase);
+    forever begin
+      @(posedge vif.out_m_mp.clk)
+      if(vif.in_m_mp.input_mcb.i_wren == 1 && vif.in_m_mp.input_mcb.i_rden == 0)begin
+        $display("\nWrite enable is high");
+        req.i_wrdata = vif.in_m_mp.input_mcb.i_wrdata;
+        req.i_wren = vif.in_m_mp.input_mcb.i_wren;
+        req.i_rden = vif.in_m_mp.input_mcb.i_rden;
+        ap.write(req);
+      end   
  
