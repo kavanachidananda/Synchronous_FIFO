5ca0a05fb8fd8c7816983db220e7d9b452acf307
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 12:08:06 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -16,7 +16,7 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
   endfunction
 
 // Run phase  
-    virtual task run_phase(uvm_phase phase);
+   virtual task run_phase(uvm_phase phase);
    if(vif.dcb.rstn==0) //check for reset
     vif.d_mp.dcb.i_wren <= 0;
     vif.d_mp.dcb.i_rden <= 0;
@@ -30,5 +30,14 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
         read();
       seq_item_port.item_done();
     end
-  endtask
+  endtask : run_phase
+
+   virtual task write(input  i_wrdata);
+    @(posedge vif.d_mp.clk)
+    vif.d_mp.dcb.i_wren <= 1;
+    vif.d_mp.dcb.i_wrdata <= i_wrdata;
+    @(posedge vif.d_mp.clk)
+   // vif.d_mp.dcb.i_wren <= 0;
+  endtask : write
+   
 endclass : fifo_driver
