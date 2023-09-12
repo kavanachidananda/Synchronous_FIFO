ad58da8c498a45f4e0d49e90dc033d8c94198bb5
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-12 15:25:50 +0530

    Update fifo_output_monitor.sv

--- fifo_output_monitor.sv
+++ fifo_output_monitor.sv
@@ -8,3 +8,21 @@ class fifo_output_monitor extends uvm_monitor;
     super.new(name, parent);
      ap = new("analysis_port", this);
   endfunction
+
+   virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+     req = f_sequence_item::type_id::create("req");
+      if(!uvm_config_db#(virtual fifo_intf)::get(this, "", "vif", vif))
+      `uvm_fatal("Monitor: ", "No vif is found!")
+  endfunction
+
+   virtual task run_phase(uvm_phase phase);
+    forever begin
+      @(posedge vif.m_mp.clk)
+      if(vif.m_mp.m_cb.wr == 1 && vif.in_)begin
+        $display("\nWR is high");
+        req.i_wrdata = vif.m_mp.m_cb.i_wrdata;
+        req.i_wren = ;
+        req.i_rden = ;
+        ap.write(req);
+      end      
