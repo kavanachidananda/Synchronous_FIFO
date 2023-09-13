09d6a99f20dfada6d3368e7b7705d980543abdc2
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-13 10:37:49 +0530

    Update fifo_passive_agent.sv

--- fifo_passive_agent.sv
+++ fifo_passive_agent.sv
@@ -1,5 +1,8 @@
 `include "fifo_output_monitor.sv"
 
 class fifo_passive_agent extends uvm_agent;
+  fifo_output_monitor o_mon;
+  'uvm_component_utils("fifo_passive_agent")
 
+  function
 endclass : fifo_passive_agent
