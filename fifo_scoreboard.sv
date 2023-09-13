7e3892225dcb2e32332c4dea4356f9ef297167bd
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-13 10:46:28 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -1,3 +1,4 @@
 class fifo_scoreboard extends uvm_scoreboard;
-
+  uvm_analysis_imp#(f_sequence_item, f_scoreboard) item_got_export;
+  `uvm_component_utils(f_scoreboard)
 endclass : fifo_scoreboard
