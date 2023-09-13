89412e881744403071c885f12dbb3cbbb15febb8
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-13 10:53:11 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -1,4 +1,14 @@
 class fifo_scoreboard extends uvm_scoreboard;
-  uvm_analysis_imp#(f_sequence_item, f_scoreboard) item_got_export;
-  `uvm_component_utils(f_scoreboard)
+  uvm_analysis_imp#(fifo_transaction, fifo_scoreboard) ap;
+  `uvm_component_utils(fifo_scoreboard)
+
+  function new(string name = "fifo_scoreboard", uvm_component parent);
+    super.new(name, parent);
+    ap = new("analysis_port", this);
+  endfunction
+
+   virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+   
 endclass : fifo_scoreboard
