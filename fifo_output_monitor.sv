1505e20f7423a22ec58b60b5be32bdc7c77d1c90
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-14 10:10:14 +0530

    Update fifo_output_monitor.sv

--- fifo_output_monitor.sv
+++ fifo_output_monitor.sv
@@ -1,12 +1,12 @@
 class fifo_output_monitor extends uvm_monitor; 
   virtual fifo_intf vif;
   fifo_transaction req;
-   uvm_analysis_port#(fifo_transaction) ap;
+  uvm_analysis_port#(fifo_transaction) ap2;
   `uvm_component_utils(fifo_output_monitor)
   
    function new(string name = "fifo_output_monitor", uvm_component parent);
     super.new(name, parent);
-     ap = new("analysis_port", this);
+     ap2 = new("analysis_port", this);
   endfunction
 
   virtual function void build_phase(uvm_phase phase);
