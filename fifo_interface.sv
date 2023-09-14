ab3061afbb5c9f3f5bfad7986de0c0c399023c0c
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-14 11:12:14 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -1,4 +1,9 @@
 interface fifo_intf(input clk,rstn);
+  parameter DATA_W   = 128;    // Data width
+  parameter DEPTH    = 1024;    // Depth of FIFO
+  parameter UPP_TH   = 4;       // Upper threshold to generate Almost-full
+  parameter LOW_TH   = 2 ;       // Lower threshold to generate Almost-empty
+
   logic i_wren,i_rden,o_full,o_empty,o_alm_full,o_alm_empty;
   logic  [DATA_W - 1 : 0] i_wrdata;
   logic  [DATA_W - 1 : 0] o_rddata;
