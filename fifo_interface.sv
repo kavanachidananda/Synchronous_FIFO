741b341af7d70ea05d8ec13e094fe652348fb4f2
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-14 16:23:37 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -7,26 +7,24 @@ interface fifo_intf(input clk,rstn);
   logic i_wren,i_rden,o_full,o_empty,o_alm_full,o_alm_empty;
   logic  [DATA_W - 1 : 0] i_wrdata;
   logic  [DATA_W - 1 : 0] o_rddata;
-endinterface : fifo_intf 
+//endinterface : fifo_intf 
 
 //driver clocking block
-clocking dcb @(posedge clk)
+  clocking dcb @(posedge clk);
   default input #1 output #1;
   output  i_wrdata;
   output i_wren,i_rden;
-  input o_rddata;
-  input o_full,o_empty,o_alm_full,o_alm_empty;
 endclocking : dcb
 
 //input monitor clocking block
-clocking input_mcb @(posedge clk)
+  clocking input_mcb @(posedge clk);
   default input #1 output #1;
   input  i_wrdata;
   input i_wren,i_rden;
 endclocking : input_mcb
 
 //output monitor clocking block
-clocking output_mcb @(posedge clk)
+  clocking output_mcb @(posedge clk);
   default input #1 output #1;
   input  o_rddata;
   input o_full,o_empty,o_alm_full,o_alm_empty;
