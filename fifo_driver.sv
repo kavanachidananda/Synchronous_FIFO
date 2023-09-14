9a6cbb9c28b2437b91392c6d0b39c85faa02da6e
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-14 16:21:49 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,13 +1,9 @@
- parameter DATA_W   = 128;    // Data width
-  parameter DEPTH    = 1024;    // Depth of FIFO
-  parameter UPP_TH   = 4;       // Upper threshold to generate Almost-full
-  parameter LOW_TH   = 2 ;       // Lower threshold to generate Almost-empty
 
 class fifo_driver extends uvm_driver #(fifo_transaction);
  virtual fifo_intf vif; //interface handle
   fifo_transaction req; //sequence_item handle
  
- `uvm_utils_component(fifo_driver)
+ `uvm_component_utils(fifo_driver)
  
  function new(string name = "fifo_driver", uvm_component parent);
     super.new(name, parent);
@@ -22,11 +18,12 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
 
 // Run phase  
    virtual task run_phase(uvm_phase phase);
-   if(vif.dcb.rstn==0) //check for reset
+    if(vif.d_mp.rstn==0) //check for reset
     vif.d_mp.dcb.i_wren <= 0;
     vif.d_mp.dcb.i_rden <= 0;
     vif.d_mp.dcb.i_wrdata <= 0;
-   endtask
+   
+    
     forever begin
       seq_item_port.get_next_item(req);
      if(req.i_wren == 1 && req.i_rden == 0)
@@ -39,14 +36,14 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
       idle();
       seq_item_port.item_done();
     end
-  endtask : run_phase
+    endtask
 
    
    virtual task write(input  [DATA_W - 1 : 0] i_wrdata);
     @(posedge vif.d_mp.clk)
     vif.d_mp.dcb.i_wren <= 1;
-    vif.d_mp.dcb.i_wrdata <= i_wrdata;
-  //  @(posedge vif.d_mp.clk)
+    vif.d_mp.dcb.i_wrdata <= req.i_wrdata;
+  // @(posedge vif.d_mp.clk)
    // vif.d_mp.dcb.i_wren <= 0;
   endtask : write
 
@@ -60,7 +57,7 @@ class fifo_driver extends uvm_driver #(fifo_transaction);
    virtual task write_and_read(input  [DATA_W - 1 : 0] i_wrdata);
      @(posedge vif.d_mp.clk)
     vif.d_mp.dcb.i_wren <= 1;
-    vif.d_mp.dcb.i_wrdata <= i_wrdata;
+    vif.d_mp.dcb.i_wrdata <= req.i_wrdata;
     vif.d_mp.dcb.i_rden <= 1;
    endtask : write_and_read
 
