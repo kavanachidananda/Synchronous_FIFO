703a6f3abee77229ea2987c82e44bd687c8e6275
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-17 17:04:02 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,8 +1,3 @@
-/* parameter DATA_W   = 128;    // Data width
-  parameter DEPTH    = 1024;    // Depth of FIFO
-  parameter UPP_TH   = 4;       // Upper threshold to generate Almost-full
-  parameter LOW_TH   = 2 ;       // Lower threshold to generate Almost-empty*/
-
 class fifo_sequence extends uvm_sequence #(fifo_transaction);
   `uvm_object_utils(fifo_sequence) //factory registration
    fifo_transaction req;
@@ -11,32 +6,32 @@ class fifo_sequence extends uvm_sequence #(fifo_transaction);
   endfunction
   
 virtual task body();
-   `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Write REQs ********"), UVM_LOW)
-   repeat(DEPTH) begin
+   `uvm_info(get_type_name(), $sformatf("******** Generate Write REQs ********"), UVM_LOW)
+  repeat(5) begin
     req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
     assert(req.randomize()with {req.i_wren==1; req.i_rden==0;});
     finish_item(req);
+    `uvm_info(" SEQUENCE : Write enable is high and Read enable is low", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0d  ",req.i_wren, req.i_rden,req.i_wrdata), UVM_LOW);
     end
-
-   `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
-    repeat(DEPTH) begin
+   `uvm_info(get_type_name(), $sformatf("******** Generate Read REQs ********"), UVM_LOW)
+  repeat(5) begin
      req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
-      assert(req.randomize()with {req.i_wren==0; req.i_rden==1;});
+      assert(req.randomize()with {i_wren==0; i_rden==1;});
     finish_item(req);
     end
 
    `uvm_info(get_type_name(), $sformatf("******** Idle condition ********"), UVM_LOW)
-  repeat(10) begin
+  repeat(1) begin
      req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
     assert(req.randomize()with {req.i_rden==0 ; req.i_wren==0;});
     finish_item(req);
     end
 
-   `uvm_info(get_type_name(), $sformatf("******** Generate Alternate read and write ********"), UVM_LOW)
-  for(int i=0;i<100;i++) begin
+  `uvm_info(get_type_name(), $sformatf("******** Generate Alternate read and write ********"), UVM_LOW)
+  for(int i=0;i<6;i++) begin
      req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
     assert(req.randomize()with {req.i_rden==0 ; req.i_wren==1;}); //write
@@ -47,7 +42,7 @@ virtual task body();
     end
 
   `uvm_info(get_type_name(), $sformatf("******** Simultaneous read and write ********"), UVM_LOW)
-  for(int i=0;i<100;i++) begin
+  for(int i=0;i<4;i++) begin
      req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
     assert(req.randomize()with {req.i_rden==1 ; req.i_wren==1;}); 
@@ -55,12 +50,12 @@ virtual task body();
     end
 
    `uvm_info(get_type_name(), $sformatf("******** Generate Random req's ********"), UVM_LOW)
-  for(int i=0;i<100;i++) begin
+  for(int i=0;i<10;i++) begin
      req = fifo_transaction::type_id::create("req");  //creating sequence_item
     start_item(req);
     assert(req.randomize());
     finish_item(req);
-    end
+    end 
 endtask
 endclass 
 
