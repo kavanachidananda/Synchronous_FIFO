c6b605f8f13eafbb6e5b11b52625ffb9bf3dc0a5
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-17 17:02:30 +0530

    Update fifo_dut.v

--- fifo_dut.v
+++ fifo_dut.v
@@ -14,7 +14,7 @@ module SYN_FIFO(
  
 
   // FIFO parameters
-  parameter DEPTH = 1024;
+  parameter DEPTH = 5;
   parameter WIDTH = 128;
 
  
@@ -23,37 +23,57 @@ module SYN_FIFO(
   reg [WIDTH-1:0] fifo[0:DEPTH-1];
   reg [9:0] write_ptr;
   reg [9:0] read_ptr;
+  reg [127:0] out;
   wire [9:0] fifo_count = write_ptr - read_ptr;
   wire almost_full_condition = (fifo_count >= (DEPTH - 4));
   wire almost_empty_condition = (fifo_count <= 2);
 // Full and Empty signals
-  assign full = (fifo_count >= (DEPTH - 1));
+  assign full = (fifo_count >= (DEPTH));
   assign almost_full = (almost_full_condition && !full);
   assign empty = (fifo_count == 0);
   assign almost_empty = (almost_empty_condition && !empty);
 
  
 
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
       write_ptr <= 0;
       read_ptr <= 0;
-    end else if (write_en) begin
+      
+    end
+    
+    
+    else  if (write_en==1 && read_en==0) begin
       fifo[write_ptr] <= data_in;
       write_ptr <= write_ptr + 1;
     end
-
+    
  
+      
 
-    if (rst) begin
-      read_ptr <= 0;
-    end else if (read_en) begin
-      read_ptr <= read_ptr + 1;
+    else if (read_en ==1 && write_en==0) begin
+
+        out <= fifo[read_ptr];
+        read_ptr <= read_ptr + 1; 
     end
+    
+    else if (write_en && read_en) begin
+      write_ptr=0;
+      read_ptr=0;
+      fifo[write_ptr] = data_in;
+      out = fifo[read_ptr];
+    end
+    
+    else if (!write_en && !read_en) begin
+      write_ptr=0;
+      read_ptr=0;
+      fifo[write_ptr] = 0;
+      out = fifo[read_ptr];
+    end 
   end
 
  
 // Assign data_out outside of procedural blocks
-  assign data_out = (read_en) ? fifo[read_ptr] : 128'b0;
+  assign data_out = out;
 
 endmodule
