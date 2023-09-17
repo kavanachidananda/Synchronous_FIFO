e62564fb9c24b57c3d0107023268b5d58a61eec0
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-17 17:05:13 +0530

    Update fifo_transaction.sv

--- fifo_transaction.sv
+++ fifo_transaction.sv
@@ -1,5 +1,5 @@
  parameter DATA_W   = 128;    // Data width
-  parameter DEPTH    = 1024;    // Depth of FIFO
+ parameter DEPTH    = 5;    // Depth of FIFO
   parameter UPP_TH   = 4;       // Upper threshold to generate Almost-full
   parameter LOW_TH   = 2 ;       // Lower threshold to generate Almost-empty
 class fifo_transaction extends uvm_sequence_item;
