e43c592f51e0c8432fa158cde5652ab33f404e29
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-09-17 17:01:54 +0530

    Update fifo_env.sv

--- fifo_env.sv
+++ fifo_env.sv
@@ -1,9 +1,7 @@
 `include "fifo_active_agent.sv"
 `include "fifo_scoreboard.sv"
-`include "fifo_passive_agent.sv"
  class fifo_env extends uvm_env;
   fifo_active_agent agt1;
-  fifo_passive_agent agt2;
   fifo_scoreboard scb;
    `uvm_component_utils(fifo_env)
   
@@ -14,13 +12,11 @@
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     agt1 = fifo_active_agent::type_id::create("agt1", this);
-    agt2 = fifo_passive_agent::type_id::create("agt2",this);
     scb = fifo_scoreboard::type_id::create("scb", this);   
   endfunction
   
   virtual function void connect_phase(uvm_phase phase); 
    agt1.in_mon.ap1.connect(scb.ap1);
-   agt2.o_mon.ap2.connect(scb.ap2);
   endfunction
   
 
